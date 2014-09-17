2f7f05e9c0252520c21fa0c13ce780291aaa5934
Eduardo Rios Jr <eddier_117@utexas.edu>
2014-09-16 23:45:06 -0500

    completed a working RMSE function

--- Netflix.py
+++ Netflix.py
@@ -1,7 +1,21 @@
 def root_mean_squared_error (x, y) :
 
-  '''
+'''
 This program will do the following:
   1.  Take 'x' the actual value and 'y' our guess, as inputs
-  2.  Subtract y - x
-  3.  
+  2.  Compute Root-Mean-Squared-Error 
+  3.  Output RMSE
+''' 
+
+  sum = 0
+  count = 0
+
+  for i in range(len(x) - 1) :
+    sum += (y[i] - x[i]) ** 2 
+    count += 1
+  
+  rmse = (sum / count) ** (1/2)
+
+  assert (5 < rmse > 0)
+
+  return rmse
