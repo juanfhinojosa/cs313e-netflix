8cf370a38479086ae305bdea3db23445818f5dcf
Eduardo Rios Jr <eddier_117@utexas.edu>
2014-09-17 22:47:22 -0500

    cache for movieid

--- test_input.py
+++ test_input.py
@@ -1,14 +1,31 @@
 def main():
 	import os
 	# Open remote directory to read data
-	for i in range (0, 17771):
-		changing_num = str(1)
+	movieid_avg_rate = {}
+	cust_avg_rate = {}
+	for i in range (0, 1000):
+		changing_num = str(i)
 		while len(changing_num) < 7:
 			changing_num = '0' + changing_num
 		base = ('/u/downing/cs/netflix/training_set/mv_' + changing_num + '.txt')
 		inFile = open(base , 'r')
-	
+
+		count = 0
+		rating = 0
+
 		for line in inFile:
-			print(line)
+			if line.find(':') != (-1) :
+				movie_id = line[:-1]
+			else:
+				customer_id = line[:-(line.find(','))]
+				line.replace(customer_id, '')
+				rating += int(line[0])
+		rating = rating / count
+
+		movieid_avg_rate['movie_id'] = 'rating'			
+
+		inFile.close()
+
+	print(movieid_avg_rate)
 main()
 
