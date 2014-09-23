a79c9d08d55344bf7b21acd03e4bdfc855fa51fc
Juan Hinojosa <juanfhinojosa@utexas.com>
2014-09-23 01:12:01 -0500

    Added cust and mov differences

--- jfh793-Netflix.py
+++ jfh793-Netflix.py
@@ -21,11 +21,40 @@ def root_mean_squared_error (x, y) :
 
 # /u/prat0318/netflix-tests/
 
+def Predict( customer , movie ):
+  global C
+  global B
+  
+  Total_Cust_Avg = 3.6741013034524364
+  Total_Mov_Avg = 3.228137194500105
+  
+  Cust_Diff = Total_Cust_Avg - C[customer]
+  Movie_Diff = Total_Mov_Avg - B[movie]
+  
+  return Cust_Diff + Movie_Diff + Total_Mov_Avg 
+
 def main():
   
+  AvgCustRt = 0
   AverageRating = open('/u/prat0318/netflix-tests/ctd446-userAverageRating.txt' , 'r')
-  Customer = eval(AverageRating.readline())
-    print(Customer)
+  C = eval(AverageRating.readline())
+  for items in C:
+    AvgCustRt += float(C[items])
+  
+  finalavg = (AvgCustRt / len(C))
+  print(finalavg)
+
+
+  
+  AvgMviRt = 0
+  AverageMviRating = open('/u/prat0318/netflix-tests/ctd446-movieAverageRating.txt' , 'r')
+  B = eval(AverageMviRating.readline())
+  for things in B:
+    AvgMviRt += float(B[things])
+  
+  MvAvg = (AvgMviRt / len(B))
+  print(MvAvg)
+
   '''
   F = open('/u/prat0318/netflix-tests/cct667-ProbeCacheAnswers.txt' , 'r')
   File = F.read()
